65c6147018699721b406c3e41dcb98d0822f5134
Amanda Duarte <amandaduart33@gmail.com>
2025-04-11 14:01:25 -0300

    Adiciona função frear ao veículo

---
+++ POOM2024.2/index.ts
@@ -0,0 +1,64 @@
+import { Veiculo } from "./Veiculo";
+import prompt from "prompt-sync";
+
+const teclado = prompt();
+
+console.log('Criação de veículo');
+const carro: Veiculo = criaVeiculo();
+
+while(true){
+    console.log("########### MENU ###########");
+    console.log("1 - Acelerar");
+    console.log("2 - Frear");
+    console.log("3 - Subir marcha");
+    console.log("4 - Descer marcha");
+    console.log("5 - Imprimir dados do veículo");
+    console.log("0 - Sair");
+
+    const opcao = +teclado('Escolha uma opção: ');
+    if(opcao === 0){
+        break;
+    }
+    switch (opcao) {
+        case 1:
+            acelerar(carro);
+            break;
+
+        case 2:
+            frear(carro);
+    
+        default:
+            break;
+    }
+}
+
+console.table(carro);
+
+function acelerar(veiculo: Veiculo): void{
+    if(veiculo.marchaAtual != 0){
+    veiculo.velocidade += veiculo.potencia*0.1;
+    console.log(veiculo.velocidade);
+}}
+
+function frear(veiculo: Veiculo): void {
+    if (veiculo.velocidade > 0) {
+        veiculo.velocidade -= veiculo.potencia * 0.05;
+        if (veiculo.velocidade < 0) {
+            veiculo.velocidade = 0;
+        }
+        console.log(`Freando: ${veiculo.velocidade.toFixed(2)} km/h`);
+    } else {
+        console.log('O veículo já está parado.');
+    }
+}
+
+function criaVeiculo(): Veiculo{
+    const veiculo: Veiculo = new Veiculo();
+    veiculo.marca = teclado('Marca: ');
+    veiculo.modelo = teclado('Modelo: ');
+    veiculo.potencia = +teclado('Potência: ');
+    veiculo.numeroMarchas = +teclado('Número de marchas: ');
+    return veiculo;
+}
+
+
